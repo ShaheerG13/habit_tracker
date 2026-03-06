15acc49f10118a5a322b3a382b357dd35c747855
ShaheerG13 <shaheerghazenfer13@gmail.com>
2026-03-05 21:24:56 -0500

    memory cache added (faster page loading)

---
+++ static/index_scripts.js
@@ -0,0 +1,20 @@
+// modal box
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("modalBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function(e) {
+  e.preventDefault();
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
